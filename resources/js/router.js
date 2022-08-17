e53d7730b43a626d275066a45e1164570837c823
donacesar <donacesar@gmail.com>
2022-08-17 09:36:06 +0300

    Prepare to jwt

---
+++ resources/js/router.js
@@ -0,0 +1,22 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter)
+
+export default new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/fruits', component: () => import('./components/Fruit/Index'),
+            name: 'fruit.index'
+        },
+        {
+            path: '/users/login', component: () => import('./components/User/Login'),
+            name: 'user.login'
+        },
+        {
+            path: '/users/registration', component: () => import('./components/User/Registration'),
+            name: 'user.registration'
+        }
+    ]
+})
