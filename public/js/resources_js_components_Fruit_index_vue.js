3aaca3107dad924d1749419d49882cdc8e2122c4
donacesar <donacesar@gmail.com>
2022-08-18 20:48:42 +0300

    Add refresh_token

--- public/js/resources_js_components_Fruit_index_vue.js
+++ public/js/resources_js_components_Fruit_index_vue.js
@@ -77,9 +77,7 @@ var api = axios__WEBPACK_IMPORTED_MODULE_0___default().create(); // start reques
 
 api.interceptors.request.use(function (config) {
   if (localStorage.getItem('access_token')) {
-    config.headers = {
-      'authorization': "Bearer ".concat(localStorage.getItem('access_token'))
-    };
+    config.headers.authorization = "Bearer ".concat(localStorage.getItem('access_token'));
   }
 
   return config;
@@ -87,10 +85,24 @@ api.interceptors.request.use(function (config) {
 // start response
 
 api.interceptors.response.use({}, function (error) {
+  if (error.response.data.message === 'Token has expired') {
+    return axios__WEBPACK_IMPORTED_MODULE_0___default().post('api/auth/refresh', {}, {
+      headers: {
+        'authorization': "Bearer ".concat(localStorage.getItem('access_token'))
+      }
+    }).then(function (res) {
+      localStorage.setItem('access_token', res.data.access_token);
+      error.config.headers.authorization = "Bearer ".concat(res.data.access_token);
+    });
+  }
+
+  console.log(error.response.data.message);
+
   if (error.response.status === 401) {
     _router__WEBPACK_IMPORTED_MODULE_1__["default"].push({
       name: 'user.login'
     });
+    return api.request(error.config);
   }
 });
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (api); // end response
--- resources/js/api.js
+++ resources/js/api.js
@@ -6,9 +6,7 @@ const api = axios.create()
 // start request
 api.interceptors.request.use(config => {
     if (localStorage.getItem('access_token')) {
-        config.headers = {
-            'authorization': `Bearer ${localStorage.getItem('access_token')}`
-        }
+        config.headers.authorization = `Bearer ${localStorage.getItem('access_token')}`
     }
     return config
 }, error => {
@@ -16,9 +14,21 @@ api.interceptors.request.use(config => {
 // end request
 
 // start response
-api.interceptors.response.use( {}, error => {
+api.interceptors.response.use({}, error => {
+    if (error.response.data.message === 'Token has expired') {
+        return axios.post('api/auth/refresh', {}, {
+            headers: {
+                'authorization': `Bearer ${localStorage.getItem('access_token')}`
+            }
+        }).then(res => {
+            localStorage.setItem('access_token', res.data.access_token)
+            error.config.headers.authorization = `Bearer ${res.data.access_token}`
+        })
+    }
+    console.log(error.response.data.message);
     if (error.response.status === 401) {
         router.push({name: 'user.login'})
+        return api.request(error.config)
     }
 })
 export default api
